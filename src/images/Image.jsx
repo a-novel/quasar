9f53ce7b532933e6bbe7d4f426c5d8f4a7648b91
Kushuh <kuzanisu@gmail.com>
2020-10-14 18:23:18 +0200

    updated styles

--- src/images/Image.jsx
+++ src/images/Image.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import css from './Image.module.css';
 
 const Image = ({src, className, ...props}) => (
-  <div className={`${css.container} ${className || ''}`} style={{backgroundImage: `url(${src})`}} {...props}/>
+  <div className={`${css.container} ${className || ''}`} style={{background: `url(${src})`}} {...props}/>
 );
 
 export default Image;
