6d1243d1deeefae0407ea71bf3a170d6738f3b88
Kushuh <kuzanisu@gmail.com>
2020-10-18 13:55:39 +0200

    fixed import issue

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import Input, {createExposer} from './forms/Input.jsx';
 import Image from './images/Image.jsx';
 import Svg from './images/Svg.jsx';
 import Layers from './display/Layers.jsx';
-import Flex from './display/Flex';
+import Flex from './display/Flex.jsx';
 
 export {Input, createExposer, Image, Svg, Layers, Flex};
